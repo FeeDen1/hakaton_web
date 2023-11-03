915d8bf7721fc4910fef5361914686fe5c0cf4e3
Denis <denisfedin1308@mail.ru>
2023-11-03 15:08:22 +0300

    Navbar

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, {useState} from "react";
+import './styles/App.css'
+import Navbar from "./components/UI/navbar/Navbar";
+function App() {
+  return (
+      <Navbar title='Муниципальное образование города-курорта Анапа'/>
+  )}
+export default App;
---
+++ src/components/UI/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+
+import cl from './Navbar.module.css'
+import MyButton from "../button/MyButton";
+
+
+const Navbar = ({title}) => {
+  return (
+      <nav className={cl.navbar}>
+        <div className={cl.navbar_left}>
+          <img width='40px' height='auto' src='https://www.anapa-official.ru/simai.data/image/logo/Coat_of_Arms_of_Anapa_(Krasnodar_krai).svg%20copy.png' alt=''/>
+          <h1>{title}</h1>
+        </div>
+        <div className={cl.navbar_right}>
+
+          <MyButton>
+            Сменить тему
+          </MyButton>
+        </div>
+      </nav>
+
+      );
+};
+
+export default Navbar;
